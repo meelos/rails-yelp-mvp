d95a7bc9402ca15865672f43315b0adc0f8e0270
Myles Kilgallon <github@myles.uk>
2019-07-31 14:50:12 +0800

    green

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
-  validates :category, inclusion: { in: [chinese, italian, japanese,french, belgian] }
+  validates :category, presence: true, inclusion: { in: ['chinese', 'italian', 'japanese','french', 'belgian'] }
 end
 
